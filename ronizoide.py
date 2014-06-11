9bddc243a1d8bf6004e77108823b0048a4069e7c
Roní Gonçalves <ronignc@gmail.com>
2014-06-11 17:26:07 -0300

    Início do projeto, inclusão do arquivo principal ronizoide.py

---
+++ ronizoide.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+
+ronizoide versão 0.1
+
+Servidor web básico feito como atividade da disciplina 
+de Redes de Computadores da Faculdade de Engenharia 
+Elétrica da Universidade de Uberlândia.
+
+O programa implementa de forma simples o protocolo 
+HTTP1.1.
+
+Autor: Roní Gilberto Gonçalves
+Número de matrícula: 10921EEL026
+
+'''
+
+print "Olá, mundo!"
+
+for i in range(1000):
+	print i
